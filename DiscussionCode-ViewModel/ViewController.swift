06e51936b35a95683882cf8277d3193a69796cc4
Michel Anderson Lutz Teixeira <michel_lutz@icloud.com>
2019-08-21 18:36:27 -0300

    Initial Commit

---
+++ DiscussionCode-ViewModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscussionCode-ViewModel
+//
+//  Created by Michel Anderson Lutz Teixeira on 21/08/19.
+//  Copyright © 2019 CocoaHeads POA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
